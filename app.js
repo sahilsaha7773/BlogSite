1f5a232666f966fcdea76f25ed8e0b3768bc0d22
sahilsaha7773 <sahilsaha7773@gmail.com>
2021-01-21 00:32:35 +0530

    Added functionality to delete blog posts

--- app.js
+++ app.js
@@ -93,6 +93,14 @@ app.get("/posts/:postName", function(req, res){
   });
 });
 
+//Post request to delete a post
+app.post("/posts/delete", function(req, res) {
+  const requestedTitle = req.body.title;
+  Blog.deleteOne({blogTitle: requestedTitle})
+  .catch(err => console.log(err));
+  res.redirect('/');
+});
+
 //Launching the server on port 3000-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
